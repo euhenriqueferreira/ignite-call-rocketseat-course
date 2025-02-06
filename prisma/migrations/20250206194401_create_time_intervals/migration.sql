c12e4f19b653c007bd86acefebdc6e1bd605542d
Henrique <henriqueferreira0320@gmail.com>
2025-02-06 17:07:15 -0300

    Time Intervals page logic finalized

---
+++ prisma/migrations/20250206194401_create_time_intervals/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - You are about to drop the `UserTimeInterval` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `created_at` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `sessions` table. All the data in the column will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "UserTimeInterval";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "user_time_intervals" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "week_day" INTEGER NOT NULL,
+    "time_start_in_minutes" INTEGER NOT NULL,
+    "time_end_in_minutes" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "user_time_intervals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_sessions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_sessions" ("expires", "id", "session_token", "user_id") SELECT "expires", "id", "session_token", "user_id" FROM "sessions";
+DROP TABLE "sessions";
+ALTER TABLE "new_sessions" RENAME TO "sessions";
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
